55e83f58df171b94011fc01b6da211e12e8406de
mati20101 <mati20102010@gmail.com>
2022-06-06 01:10:40 +0200

    Update

---
+++ xamarin/App/App/App.Android/MainActivity.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.OS;
+using Android.Content;
+using System.IO;
+using System.Runtime.CompilerServices;
+using Android.Util;
+using Xamarin.Essentials;
+
+namespace App.Droid
+{
+    [Activity(Label = "App", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        public static MainActivity Instance;
+        public static int PickImageId;
+
+        public static string PhotoPath { get; private set; }
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            App thisApp = new App();
+
+            LoadApplication(thisApp);
+            // MediaPicker.PickPhotoAsync();
+            Instance = this;
+            
+        }
+        
+
+
+    }
+}
---
+++ xamarin/App/App/App/MainPage.xaml.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace App
+{
+    public partial class MainPage : ContentPage
+    {
+        public static MainPage Instance;
+        public MainPage()
+        {
+            InitializeComponent();
+            Instance = this;
+            System.Uri uri;
+            System.Uri.TryCreate("http://192.168.0.242/php/photos/Piekny_Obraz.png", UriKind.Absolute, out uri);
+
+            Task<ImageSource> result = Task<ImageSource>.Factory.StartNew(() => ImageSource.FromUri(uri));
+            result.Wait();
+            img.Source = (ImageSource)result.Result;
+            // Define the Intent for getting images
+            
+        }
+        public static void h1(string message)
+        {
+
+            Instance.img.Source = (ImageSource)ImageSource.FromFile(message); ;
+        }
+        public static  void Button_Clicked(object sender, EventArgs e)
+        {
+
+            
+        }
+
+        private  async void Button_Clicked_1(object sender, EventArgs e)
+        {
+            FileResult picker = await  MediaPicker.PickPhotoAsync();
+            string resultFileName = await DisplayPromptAsync("Question 1", "Jak ma się nazywać?");
+            string resultWho = await DisplayPromptAsync("Question 2", "Kto wysyła?");
+            DisplayAlert("AA", picker.FullPath, "OK");
+            send(picker.FullPath, resultFileName, resultWho);
+        }
+
+        private void send(string path, string name, string who)
+        {
+            Task.Run(() =>
+            {
+                byte[] bytes = System.IO.File.ReadAllBytes(path);
+                Encoding encoding = Encoding.GetEncoding("ISO-8859-1");
+                string byteMessage = encoding.GetString(bytes);
+                HttpWebRequest Uploadpage = HttpWebRequest.CreateHttp("http://192.168.0.242/php/API/UploadImage.php");
+                Uploadpage.Method = "POST";
+                //Uploadpage.ContentLength =9999;
+                Uploadpage.ContentType = "multipart/form-data; boundary=----WebKitFormBoundaryA88cFFBr8SgBd3kO";
+
+                StreamWriter writer = new StreamWriter(Uploadpage.GetRequestStream(), Encoding.GetEncoding("ISO-8859-1"));
+
+
+                writer.WriteLine("------WebKitFormBoundaryA88cFFBr8SgBd3kO");
+                writer.WriteLine("Content-Disposition: form-data; name=\"fileToUpload\"; filename=\"plik.png\"");
+                writer.WriteLine("Content-Type: image/png");
+                writer.Write("\r\n");
+                writer.WriteLine(byteMessage);
+                writer.WriteLine("------WebKitFormBoundaryA88cFFBr8SgBd3kO");
+                writer.WriteLine("Content-Disposition: form-data; name=\"Name\"");
+                writer.Write("\r\n");
+                writer.WriteLine(name);
+                writer.WriteLine("------WebKitFormBoundaryA88cFFBr8SgBd3kO");
+                writer.WriteLine("Content-Disposition: form-data; name=\"Who\"");
+                writer.Write("\r\n");
+                writer.WriteLine(who);
+                writer.WriteLine("------WebKitFormBoundaryA88cFFBr8SgBd3kO");
+                writer.WriteLine("Content-Disposition: form-data; name=\"submit\"");
+                writer.Write("\r\n");
+                writer.WriteLine("------WebKitFormBoundaryA88cFFBr8SgBd3kO--");
+                writer.Flush();
+
+                HttpWebResponse UpliadPageResponse = (HttpWebResponse)Uploadpage.GetResponse();
+                StreamReader reader = new StreamReader(UpliadPageResponse.GetResponseStream());
+               Console.WriteLine(reader.ReadToEnd());
+            });
+            
+
+        }
+    }
+}
